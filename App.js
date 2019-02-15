568c65c3baac099255c2d6eda0820d1f4f4c7b83
shrekuu <shrekuu@163.com>
2019-02-15 18:39:42 +0800

    tries 14

--- App.js
+++ App.js
@@ -40,7 +40,7 @@ class App extends Component<Props> {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>This is the 13th time.</Text>
+        <Text style={styles.welcome}>This is the 14th time.</Text>
         <TouchableOpacity onPress={this.onButtonPress}>
           <Text>Check for updates</Text>
         </TouchableOpacity>
